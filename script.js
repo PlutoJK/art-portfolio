31d4b68d65ab3dce5a264d94ec8ae5d7a03bd914
PlutoJK <121587935+PlutoJK@users.noreply.github.com>
2022-12-28 16:36:39 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('add-image-form');
+const imagesContainer = document.getElementById('images');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const imageUrl = document.getElementById('image-url').value;
+  const imageName = imageUrl.split('/').pop();
+  const apiUrl = 'https://api.github.com/repos/PlutoJK/art-portfolio/contents/' + imageName;
+
+  const response = await fetch(apiUrl, {
+    method: 'PUT',
+    headers: {
+      'Authorization': 'Bearer <your-personal-access-token>',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'message': 'Add image ' + imageName,
+      'content': btoa(await (await fetch(imageUrl)).text())
+    })
+  });
+
+  if (response.ok) {
+    const image = document.createElement('img');
+    image.src = imageUrl;
+    imagesContainer.appendChild(image);
+  }
+});
